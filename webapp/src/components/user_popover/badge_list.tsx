f0e3cebe55fbc8d8825f5cd9d0d5f278c7d10134
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-01-27 13:54:07 +0000

    fix twitch

--- webapp/src/components/user_popover/badge_list.tsx
+++ webapp/src/components/user_popover/badge_list.tsx
@@ -14,6 +14,8 @@ import {RHSState} from 'types/general';
 import {IMAGE_TYPE_EMOJI, RHS_STATE_DETAIL, RHS_STATE_MY, RHS_STATE_OTHER} from '../../constants';
 import {markdown} from 'utils/markdown';
 
+import './badge_list.scss';
+
 type Props = {
     debug: GlobalState;
     user: UserProfile;
@@ -35,8 +37,8 @@ type State = {
     loaded?: Boolean;
 }
 
-const MAX_BADGES = 20;
-const BADGES_PER_ROW = 10;
+const MAX_BADGES = 7;
+const BADGE_SIZE = 24;
 
 class BadgeList extends React.PureComponent<Props, State> {
     constructor(props: Props) {
@@ -109,7 +111,7 @@ class BadgeList extends React.PureComponent<Props, State> {
         let row: React.ReactNode[] = [];
         for (let i = 0; i < toShow; i++) {
             const badge = this.state.badges![i];
-            if (i !== 0 && i % BADGES_PER_ROW === 0) {
+            if (i !== 0) {
                 content.push((<div>{row}</div>));
                 row = [];
             }
@@ -133,7 +135,7 @@ class BadgeList extends React.PureComponent<Props, State> {
                         <a onClick={() => this.onBadgeClick(badge)}>
                             <BadgeImage
                                 badge={badge}
-                                size={24}
+                                size={BADGE_SIZE}
                             />
                         </a>
                     </span>
@@ -145,24 +147,36 @@ class BadgeList extends React.PureComponent<Props, State> {
         let andMore: React.ReactNode = null;
         if (nBadges > MAX_BADGES) {
             andMore = (
-                <a onClick={this.onMoreClick}>
-                    <div>{`and ${nBadges - MAX_BADGES} more`}</div>
-                </a>
+                <OverlayTrigger
+                    overlay={<Tooltip>{`and ${nBadges - MAX_BADGES} more. Click to see all.`}</Tooltip>}
+                >
+                    <button
+                        id='showMoreButton'
+                        onClick={this.onMoreClick}
+                    >
+                        <span className={'fa fa-angle-right'}/>
+                    </button>
+                </OverlayTrigger>
             );
         }
+        const maxWidth = (MAX_BADGES * BADGE_SIZE) + 30;
         let loading: React.ReactNode = null;
         if (!this.state.loaded) {
             loading = (
 
-                // Reserve enough height for two rows of badges and the "and more" link
-                <div style={{height: 64}}>{'Loading...'}</div>
+                // Reserve enough height one row of badges and the "and more" button
+                <div style={{height: BADGE_SIZE, minWidth: 66, maxWidth}}>
+                    {'Loading...'}
+                </div>
             );
         }
         return (
-            <div>
+            <div id='badgeList'>
                 <div><b>{'Badges'}</b></div>
-                {content}
-                {andMore}
+                <div id='contentContainer' >
+                    {content}
+                    {andMore}
+                </div>
                 {loading}
                 <a onClick={this.onGrantClick}>
                     <div>{'Grant badge'}</div>
